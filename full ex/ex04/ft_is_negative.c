4f8e9940f7fff1f7b7606cf5ccaef593b05823ab
Lisa Jongmans <ljongman@e-r11-p6.s19.be>
2019-07-07 00:08:40 +0200

    last one

---
+++ full ex/ex04/ft_is_negative.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/06 15:51:29 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/06 17:16:50 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char n)
+{
+	write(1, &n, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+	{
+		ft_putchar('P');
+	}
+	else
+	{
+		ft_putchar('N');
+	}
+}
+
+int		main(void)
+{
+	ft_is_negative(0);
+	return (0);
+}
